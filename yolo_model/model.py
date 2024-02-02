ac6385b7dbe11e373e9b6a7d2deac004a441f34f
sergiolozav <sergiolozav@gmail.com>
2024-02-02 19:37:11 -0400

    fix: Results solo se iteraba una vez

--- yolo_model/model.py
+++ yolo_model/model.py
@@ -6,9 +6,16 @@ model = YOLO("yolov8s.pt")
 
 def predict(image):
     results = model(image, conf=0.6, verbose=False, stream=True)
+    results = copy_generator(results)
     draw_predictions(image, results)
-    return image
+    return image, results
 
+def copy_generator(results):
+    copy = []
+    for r in results:
+        copy.append(r)
+    return copy
+    
 def draw_predictions(image, results):
     for r in results:
         boxes = r.boxes
