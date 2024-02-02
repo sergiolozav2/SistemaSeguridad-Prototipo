85742a877e5e9e6771fc15794f52e7fc6ebecaae
sergiolozav <sergiolozav@gmail.com>
2024-02-02 19:44:17 -0400

    refactor: Eliminados archivos sin uso

--- supabase_client.py
+++
@@ -1,5 +0,0 @@
-from supabase import create_client, Client
-
-url: str = "https://idsiaicrrggghjqchggf.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlkc2lhaWNycmdnZ2hqcWNoZ2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY3NDkzMDgsImV4cCI6MjAyMjMyNTMwOH0.7R-Ep8Hbb_Vs5e5GXyx_3sqvCV7tNU4F5uoo05vn65o"
-database: Client = create_client(url, key)
