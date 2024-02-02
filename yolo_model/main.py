671b2ad8ec24ca20f22771d858e606537a3efdf0
sergiolozav <sergiolozav@gmail.com>
2024-02-02 19:38:03 -0400

    feat: Sistema de camara registra fotos a supabase

--- yolo_model/main.py
+++ yolo_model/main.py
@@ -2,7 +2,7 @@ import cv2
 from model import predict
 from camera_feed_websocket import CameraFeedWebSocket
 import image_base64
-
+from services import alarm
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -14,10 +14,14 @@ ws_client = CameraFeedWebSocket('ws://localhost:6500/2')
 while True:
     _, image = cap.read()
 
-    image = predict(image)
+    image, results = predict(image)
     image_url = image_base64.convert(image)
     ws_client.send(image_url)
-    
+
+    dangerous_item = alarm.check_dangerous_item(results)
+    if dangerous_item:
+        alarm.register_event(dangerous_item, image_url)
+
     cv2.imshow('Webcam', image)
     if cv2.waitKey(1) == ord('q'):
         break
