ea41c0852b7dada4ca5381262caaf74df844e31c
sergiolozav <sergiolozav@gmail.com>
2024-02-02 20:47:16 -0400

    feat: Mejor sidebar con ThemedLayoutV2

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,22 +1,9 @@
-import { Layout, theme } from "antd";
-import { AppMenu } from "./AppMenu";
+import { ThemedLayoutV2 } from "@refinedev/antd";
 import { Logo } from "./Logo";
-
-const { Header, Content, Sider } = Layout;
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 export function AppLayout(props: LayoutProps) {
-  return (
-    <Layout className="min-h-screen">
-      <Sider theme="light" collapsible collapsed={false}>
-        <div className="my-8 flex items-center justify-center">
-          <Logo />
-        </div>
-        <AppMenu />
-      </Sider>
-      <Content className="mt-4 mx-4">{props.children}</Content>
-    </Layout>
-  );
+  return <ThemedLayoutV2 Title={Logo}>{props.children}</ThemedLayoutV2>;
 }
