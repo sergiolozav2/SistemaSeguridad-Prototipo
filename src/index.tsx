bcc05056aa57ad9c1a4c561e1fe56a9b3824c80f
sergiolozav <sergiolozav@gmail.com>
2024-02-02 20:49:04 -0400

    fix: Removido StrictMode por problema con WS

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import './index.css'
+import "./index.css";
 import App from "./App";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
